2bafa0e745291df03e934c9dbec7f45cc3fd13e5
Siraj Shaukat <Siraj Shaukat>
2016-06-06 01:01:50 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Siraj on 2016-06-03.
+ */
+ var app = require('express')();
+var http  = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+io.on('connection',function(socket){
+    console.log('a user is connected');
+    socket.on('disconnect',function(){
+       console.log('user is disconnected');
+    });
+});
